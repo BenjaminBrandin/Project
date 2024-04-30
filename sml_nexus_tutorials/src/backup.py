700b9ec33e206f5409bd7679e36152d23c684fd8
BenjaminBrandin <bbrandin@kth.se>
2024-04-30 15:54:09 +0200

    aetrnht

---
+++ sml_nexus_tutorials/src/backup.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python3
+import sys
+import rospy
+import copy
+from builders import BarrierFunction
+from typing import List, Dict
+import casadi as ca
+import casadi.tools as ca_tools
+import numpy as np
+from geometry_msgs.msg import Twist, PoseStamped, TransformStamped, Vector3Stamped
+from custom_msg.msg import task_msg
+import tf2_ros
+import tf2_geometry_msgs
+from tf.transformations import euler_from_quaternion
+from manager import agents, barriers
+
+
+class Controller():
+
+    def __init__(self):
+
+        # Initialize the node
+        rospy.init_node('controller')
+
+        # Optimization Problem
+        self.solver = None
+        self.parameters = None
+        self.input_vector = ca.MX.sym('input', 2)
+        self.slack_variables = {}
+
+        # Nabla variables
+        self.nabla_funs = []
+        self.nabla_inputs = []
+        self.nabla_val = []
+        
+        # Agent Information
+        self.agent_pose = PoseStamped()
+        self.agent_name = rospy.get_param('~robot_name')
+        self.agent_id = int(self.agent_name[-1])
+        self.last_received_pose = rospy.Time()
+
+        # Neighbouring Agents
+        self.agents = agents
+        # self.agents = Manager().get_agents()
+        self.neighbour_agents = {}
+
+        # Barriers
+        self.barriers = barriers
+        # self.barriers = Manager().get_barriers()
+        self.relevant_barriers = [barrier for barrier in self.barriers if self.agent_id in barrier.contributing_agents]
+        
+        # Velocity Command Message
+        self.max_velocity = 5
+        self.vel_cmd_msg = Twist()
+
+        self.task = task_msg()
+
+
+        # Setup subscribers
+        rospy.Subscriber("/qualisys/"+self.agent_name+"/pose", PoseStamped, self.pose_callback)
+        for id in self.agents:
+            if id != self.agent_id:
+                rospy.Subscriber(f"/nexus{id}/agent_pose", PoseStamped, self.other_agent_pose_callback)
+            else:
+                continue
+
+        rospy.Subscriber("/tasks", task_msg, self.task_callback)
+
+        # Setup publishers
+        self.vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=100)
+        self.agent_pose_pub = rospy.Publisher(f"agent_pose", PoseStamped, queue_size=100)
+            
+        #Setup transform subscriber
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        while not self.tf_buffer.can_transform('mocap', self.agent_name, rospy.Time()):
+            rospy.loginfo("Wait for transform to be available")
+            rospy.sleep(1)
+
+        # Initialize the optimization problem
+        self.solver = self.get_qpsolver_and_parameter_structure()
+        self.control_loop()
+
+
+
+    def get_qpsolver_and_parameter_structure(self):
+
+        # Create the parameter structure for the optimization problem --- 'p' ---
+        parameter_list = []
+        parameter_list += [ca_tools.entry(f"state_{self.agent_id}", shape=2)]
+        parameter_list += [ca_tools.entry(f"state_{id}", shape=2) for id in self.agents if id != self.agent_id] 
+        parameter_list += [ca_tools.entry("time", shape=1)]
+        self.parameters = ca_tools.struct_symMX(parameter_list)
+
+        # Create the constraints for the optimization problem --- 'g' ---
+        barrier_constraints = self.generate_barrier_constraints(self.relevant_barriers)
+        slack_constraints = - ca.vertcat(*list(self.slack_variables.values()))
+        constraints = ca.vertcat(barrier_constraints, slack_constraints)
+
+        # Create the decision variables for the optimization problem --- 'x' ---
+        slack_vector = ca.vertcat(*list(self.slack_variables.values()))
+        opt_vector = ca.vertcat(self.input_vector, slack_vector)
+
+        # Create the object function for the optimization problem --- 'f' ---
+        cost = self.input_vector.T @ self.input_vector
+        for id,slack in self.slack_variables.items():
+            if id == self.agent_id:
+                cost += 100* slack**2  
+            else :
+                cost += 10* slack**2
+
+        # Create the optimization solver
+        qp = {'x': opt_vector, 'f': cost, 'g': constraints, 'p': self.parameters}
+        solver = ca.qpsol('sol', 'qpoases', qp, {'printLevel': 'none'})
+
+        return solver
+
+        
+
+    def generate_barrier_constraints(self, barrier_list:List[BarrierFunction]) -> ca.MX:
+        constraints = []
+        for barrier in barrier_list:
+
+            # Check if the barrier has more than one contributing agent
+            if len(barrier.contributing_agents) > 1:
+                if barrier.contributing_agents[0] == self.agent_id: # can maybe use the list as priority. so the first element is the leader agent
+                    neighbour_id = barrier.contributing_agents[1]
+                else:
+                    neighbour_id = barrier.contributing_agents[0]
+            else : 
+                neighbour_id = self.agent_id
+
+            # Create the named inputs for the barrier function
+            named_inputs = {"state_"+str(id):self.parameters["state_"+str(id)] for id in barrier.contributing_agents}
+            named_inputs["time"] = self.parameters["time"]
+
+            # Get the necessary functions from the barrier
+            nabla_xi_fun = barrier.gradient_function_wrt_state_of_agent(self.agent_id)
+            partial_time_derivative_fun = barrier.partial_time_derivative
+            barrier_fun = barrier.function
+
+            # Calculate the symbolic expressions for the barrier constraint
+            nabla_xi = nabla_xi_fun.call(named_inputs)["value"]                                        
+            dbdt = partial_time_derivative_fun.call(named_inputs)["value"]                             
+            alpha_b = barrier.associated_alpha_function(barrier_fun.call(named_inputs)["value"])       
+
+            # Create load sharing for different constraints
+            if neighbour_id == self.agent_id:
+                slack = ca.MX.sym('slack', 1)
+                self.slack_variables[self.agent_id] = slack
+                load_sharing = 1
+            else:
+                slack = ca.MX.sym('slack', 1)
+                self.slack_variables[neighbour_id] = slack
+                load_sharing = 0.1
+            
+            barrier_constraint = -1* ( ca.dot(nabla_xi.T, self.input_vector) + load_sharing * (dbdt + alpha_b + slack))
+            constraints.append(barrier_constraint)
+            self.nabla_funs.append(nabla_xi_fun)
+            self.nabla_inputs.append(named_inputs)
+
+            return ca.vertcat(*constraints)
+  
+
+
+    def control_loop(self):
+        r = rospy.Rate(50)
+        
+        while not rospy.is_shutdown():
+
+            inputs_list = {}
+            # Fill the structure with the current values
+            current_parameters = self.parameters(0)
+            current_parameters["time"] = ca.vertcat(self.last_received_pose.to_sec())
+            current_parameters[f'state_{self.agent_id}'] = ca.vertcat(self.agent_pose.pose.position.x, self.agent_pose.pose.position.y)
+            for id in self.neighbour_agents.keys():
+                current_parameters[f'state_{id}'] = ca.vertcat(self.neighbour_agents[id].pose.position.x, self.neighbour_agents[id].pose.position.y)
+
+            # Calculate the gradient values
+            self.nabla_val = []
+            for i, nabla_fun in enumerate(self.nabla_funs):
+                inputs_list = {key: current_parameters[key] for key in self.nabla_inputs[i].keys()}
+                nabla_val = nabla_fun.call(inputs_list)["value"]
+                self.nabla_val.append(ca.norm_2(nabla_val))
+            # rospy.loginfo(f"gradient{self.agent_id}: {self.nabla_val}")
+
+            # Solve the optimization problem and 
+            if any(ca.norm_2(val) < 1e-10 for val in self.nabla_val):
+                optimal_input = ca.MX([0, 0])
+            else:
+                sol = self.solver(p = current_parameters, ubg = 0) 
+                optimal_input  = sol['x']
+            # rospy.loginfo(f"Optimal_input {self.agent_id}: {sol['x']}")
+
+            # Publish the velocity command 
+            # optimal_input = ca.fmin(optimal_input, self.max_velocity)
+            self.vel_cmd_msg.linear.x = optimal_input[0]
+            self.vel_cmd_msg.linear.y = optimal_input[1]        
+
+            try:
+                # Get transform from mocap frame to agent frame
+                transform = self.tf_buffer.lookup_transform('mocap', self.agent_name, rospy.Time())
+                vel_cmd_msg_transformed = transform_twist(self.vel_cmd_msg, transform)
+                self.vel_pub.publish(vel_cmd_msg_transformed)
+            except:
+                continue
+
+            r.sleep()
+
+
+    # ----------- Callbacks -----------------
+    
+    def pose_callback(self, msg):
+        self.agent_pose = msg
+        self.last_received_pose = rospy.Time.now()
+        self.agent_pose_pub.publish(self.agent_pose)
+
+    def other_agent_pose_callback(self, msg):
+        agent_id = int(msg._connection_header['topic'].split('/')[-2].replace('nexus', '')) 
+        self.neighbour_agents[agent_id] = msg
+
+    def task_callback(self, msg):
+        self.task = msg
+        rospy.loginfo(f"{self.agent_name} received task message: {self.task}")
+          
+
+
+def transform_twist(twist = Twist, transform_stamped = TransformStamped):
+
+    transform_stamped_ = copy.deepcopy(transform_stamped)
+    #Inverse real-part of quaternion to inverse rotation
+    transform_stamped_.transform.rotation.w = - transform_stamped_.transform.rotation.w
+
+    twist_vel = Vector3Stamped()
+    twist_rot = Vector3Stamped()
+    twist_vel.vector = twist.linear
+    twist_rot.vector = twist.angular
+    out_vel = tf2_geometry_msgs.do_transform_vector3(twist_vel, transform_stamped_)
+    out_rot = tf2_geometry_msgs.do_transform_vector3(twist_rot, transform_stamped_)
+
+    new_twist = Twist()
+    new_twist.linear = out_vel.vector
+    new_twist.angular = out_rot.vector
+
+    return new_twist
+
+
+
+
+if __name__ == "__main__":
+    controller = Controller()
+    try:
+        rospy.spin()
+    except ValueError as e:
+        rospy.logerr(e)
+        sys.exit(0)
+    except rospy.ROSInterruptException:
+        print("Shutting down")
+        sys.exit(0)
---
+++ sml_nexus_tutorials/src/decomposition_module.py
@@ -0,0 +1,246 @@
+# import numpy as np
+# import casadi as ca
+# import itertools
+# from builders import StlTask, TimeInterval, TemporalOperator, AlwaysOperator, EventuallyOperator, PredicateFunction
+# from graph_module import EdgeTaskContainer
+# import networkx as nx
+# from typing import Tuple , List, Dict
+
+# globalOptimizer = ca.Opti()
+
+
+# def edgeSet(path:list,isCycle:bool=False) -> List[(int,int)] :
+#     """Given a list of nodes, it returns the edges of the path as (n,n+1) 
+#     Inputs 
+#     ------------------------------------------------------------------------------------------------
+#     path (list<float>) : list of nodes in the path
+    
+#     Output 
+#     ------------------------------------------------------------------------------------------------
+#     edges (list<tuple>): list of tuples (n,n+1)
+#     """
+
+#     if not isCycle :
+#       edges = [(path[i],path[i+1]) for i in range(len(path)-1)]
+#     elif isCycle : # due to how networkx returns edges
+#       edges = [(path[i],path[i+1]) for i in range(-1,len(path)-1)]
+        
+    
+#     return edges
+
+
+# def pathMinkowskiSumVertices(listOfFormulas : List[StlTask], edgeList: List[(int,int)]) -> ca.MX  :
+#     """
+#     Given a set of edges with assigned formulas, it computes the vertices of the minkowski sum for the superlevel sets of the given formulas.
+#     Each formula is define alon one edge of the edgeList in the same sequence as the formulas are given. The edge information is used to 
+#     decide in which verse we should take the formulas if their direction of definiton is different from the direction of the path for example
+    
+#     Inputs
+#     --------------------------------------------------
+#     listOfFormulas (list<STLFormula>)
+    
+#     Outputs
+#     -----------------------------------------
+#     minkowshySumVertices (cvxpy.Variable): returns a matrix where each column is a vertex of the Minkowski sum of the hypercubes defined by each edge in the list of edges
+#     """
+    
+#     if len(listOfFormulas) != len(edgeList) :
+#         raise ValueError("list of formulas must have the same length of edges list")
+    
+#     stateSpaceDimension   = listOfFormulas[0].predicate.stateSpaceDimension
+#     cartesianProductSets  = [[-1,1],]*stateSpaceDimension
+#     hypercubeVertices     = np.array(list(itertools.product(*cartesianProductSets))).T # All vertices of hypercube centered at the origin (Each vertex is a column)
+    
+#     center = 0 # center of the hypercube
+#     nuSum  = 0 # dimensions of the hypercube
+    
+#     for formula,edgeTuple in zip(listOfFormulas,edgeList) :
+    
+#         if formula.edgeTuple != edgeTuple and formula.edgeTuple!= (edgeTuple[1],edgeTuple[0]) :
+#             raise ValueError("edge along the path is not maching the corresponding formula. Make sure that the edges order and the formulas order is correct")
+        
+#         elif formula.edgeTuple != edgeTuple : # case in which the directions are not matching
+#             if formula.predicate.isParametric :
+#                 center = center - formula.centerVar
+#                 nuSum  = nuSum  + formula.nuVar
+#             else : # case in which the formula is not parameteric 
+#                 if formula.predicate._isApproximated :
+#                     center = center - formula.predicate.optimalApproximationCenter
+#                     nuSum  = nuSum  + formula.predicate.optimalApproximationNu
+#                 else :
+#                     raise Exception("formula does not have an approximation available. PLease replace formula with its approximation before calling this method")
+#         else : # case in which the directions are matching
+#             if formula.predicate.isParametric :
+#                 center = center + formula.centerVar
+#                 nuSum  = nuSum  + formula.nuVar
+#             else : # case in which the formula is not parameteric 
+#                 if formula.predicate._isApproximated :
+#                     center = center + formula.predicate.optimalApproximationCenter
+#                     nuSum  = nuSum  + formula.predicate.optimalApproximationNu
+#                 else :
+#                     raise Exception("formula does not have an approximation available. PLease replace formula with its approximation before calling this method")
+        
+#     minkowshySumVertices = center + hypercubeVertices*nuSum/2 # find final hypercube dimension
+    
+#     return minkowshySumVertices
+
+
+# def computeNewTaskGraph(task_graph:nx.Graph, comm_graph:nx.Graph, task_edges:List[Tuple], comm_info:Dict[str, Dict], problemDimension = 2, maxInterRobotDistance = 3)-> nx.Graph: 
+#     """ Solves the task decomposition completely"""
+    
+#     numberOfVerticesHypercube = 2**problemDimension
+#     originalTaskGraph = task_graph.copy()
+
+#     pathsList                   : list[list[int]] = []
+#     pathConstraints             : list[ca.MX] = []
+#     overloadingConstraints      : list[ca.MX] = []
+#     cyclesConstraints           : list[ca.MX] = []
+#     maxCommunicationConstraints : list[ca.MX] = []
+#     positiveNuConstraint        : list[ca.MX] = []
+
+#     decompositionOccurred = False
+#     decompositionSolved = []
+
+#     for task_name, task_dict in enumerate(comm_info.items()):
+
+#         edge = task_dict["EDGE"]
+#         edge_container: EdgeTaskContainer = task_graph[edge[0]][edge[1]]["container"] # graph[edge[0]][edge[1]]["container"] is an instance of EdgeTaskContainer and have a .task_list
+#         has_tasks = len(edge_container.task_list) > 0
+#         isCommunicating = task_dict["COMMUNICATE"]
+
+#         if (not isCommunicating) and (has_tasks): 
+#             decompositionOccurred = True
+            
+#             # retrive all the formulas to be decomposed
+#             formulasToBeDecomposed: list[StlTask] = [task for task in edge_container.task_list]
+            
+            
+#             # path finding and grouping nodes
+#             path = nx.shortest_path(comm_graph,source=edge[0],target=edge[1]) # path of agents from start to end
+#             pathsList.append(path) # save sources list for later plotting
+#             # for each formula to be decomposed we will have n subformulas with n being the length of the path we select.
+
+#             for formula in formulasToBeDecomposed : # add a new set of formulas for each edge
+#                 edgeSubformulas : list[StlTask] = [] # list of subformulas associate to one orginal formula. you have as many subformulas as number of edges
+                
+          
+
+
+#                 originalTemporalOperator       = formula.temporal_operator                      # get time interval of the orginal operator
+#                 originalPredicate              = formula.predicate                    # get the predicate function
+#                 originalEdgeTuple              = tuple(formula.contributing_agents)             # get the edge tuple
+                
+#                 if originalEdgeTuple == (edge[0],edge[1]) : #case the direction is correct
+#                     edgesThroughPath = edgeSet(path=path) # find edges along the path
+#                 else :
+#                     edgesThroughPath =  edgeSet(path=path[::-1]) # we reverse the path. This is to follow the specification direction
+                
+                
+#                 for sourceNode,targetNode in edgesThroughPath:
+                    
+#                     # create a new parameteric subformula object
+#                     subformula = StlTask(predicate=PredicateFunction(), temporal_operator=originalTemporalOperator)
+                    
+#                     # warm start of the variables involved in the optimization TODO: check if you have a better warm start base on the specification you have. Maybe some more intelligen heuristic
+#                     globalOptimizer.set_initial(subformula.centerVar , task_graph.nodes[targetNode]["pos"]-task_graph.nodes[sourceNode]["pos"]) 
+#                     globalOptimizer.set_initial(subformula.nuVar   , np.ones(problemDimension)*4)
+
+#                     # add subformulas to the current path
+#                     edgeSubformulas.append(subformula)
+#                     subformulaVertices = subformula.getHypercubeVertices(sourceNode=sourceNode,targetNode=targetNode)
+                    
+#                     # set positivity of dimensions vector nu
+#                     positiveNuConstraint.append(-np.eye(problemDimension)@subformula.nuVar<=np.zeros((problemDimension,1))) # constraint on positivity of the dimension variable
+#                     task_graph[sourceNode][targetNode]["container"].add_tasks(subformula) # add the subformula to the edge container
+
+
+#                     # Set maximum distance constraint for each hypercube vertex
+#                     if maxInterRobotDistance != None :
+#                         for jj in range(numberOfVerticesHypercube) : 
+#                             maxCommunicationConstraints.append(ca.norm_2(subformulaVertices[:,jj])<=maxInterRobotDistance)     
+                   
+                
+#                 # now set that the final i sum has to stay inside the original predicate
+#                 minowkySumVertices  = pathMinkowskiSumVertices( edgeSubformulas ,  edgesThroughPath)  # return the symbolic vertices f the hypercube to define the constraints
+#                 for jj in range(numberOfVerticesHypercube) :
+#                     # ======= Does not Work with my predicates ========
+#                     pathConstraints.append(originalPredicate.function(minowkySumVertices[:,jj])<=0) # for each vertex of the minkowski sum ensure they are inside the original predicate superlevel-set
+            
+#             decompositionSolved.append((path,edgeSubformulas))    
+
+#             # mark all the used edges for the optimization
+#             edgesThroughPath = edgeSet(path=path) # find edges along the path
+#             # flag the edges applied for the optimization 
+#             for sourceNode,targetNode in   edgesThroughPath :
+#                     graph.edges[sourceNode,targetNode]["edgeObj"].flagOptimizationInvolvement()
+            
+            
+#     # Now we check the cycle constraints on the graph as a first step and we then check the overloading constraints as a second step
+#     TaskGraph       = graph.copy()
+#     noTaskEdges     = [(i,j) for i,j,attr in edges if not attr["edgeObj"].hasSpecifications] 
+#     noCommunication = [(i,j) for i,j,attr in edges if not attr["edgeObj"].isCommunicating] # because I have rewritten those specification
+#     TaskGraph.remove_edges_from(noTaskEdges)
+#     TaskGraph.remove_edges_from(noCommunication)
+    
+#     if decompositionOccurred :
+#         # adding cycles constraints to the optimization problem
+#         cycles :list[list[int]]   = sorted(nx.simple_cycles(TaskGraph))
+#         cycles = [cycle for cycle in cycles if len(cycle)>1] # eliminate self loopscycles)
+#         for omega in cycles :
+#             cycleEdges    = edgeSet(omega,isCycle=True)
+#             cycleEdgesObj :list[list[GraphEdge]] = [graph.edges[i,j]["edgeObj"] for i,j in cycleEdges ] 
+#             cyclesConstraints += createCycleClosureConstraint(cycleEdgeObjs=cycleEdgesObj,cycleEdges=cycleEdges)
+            
+#         # now we compute the overloading constraints on a single objects
+#         # one line of overloading constraints
+#         optimisedEdges = [(i,j,edgeDict["edgeObj"]) for i,j,edgeDict in graph.edges(data=True) if edgeDict["edgeObj"].isInvolvedInOptimization]
+#         for i,j,edgeObj in optimisedEdges  :
+#             print(i,j)
+#             overloadingConstraints += computeOverloadingConstraints(edgeObj)
+        
+                
+#         # #########################################################################################################
+#         # # OPTIMIZATION
+#         # #########################################################################################################
+
+#         cost = 0 # compute cost for parameetric formulas
+#         for i,j,edgeObj in optimisedEdges :
+#             for formula in edgeObj.formulasList :
+#                 if formula.isParametric :
+#                     cost = cost + 1/computeVolume(formula.nuVar)
+            
+            
+#         constraints = [*maxCommunicationConstraints,*positiveNuConstraint,*pathConstraints,*cyclesConstraints,*overloadingConstraints]
+#         globalOptimizer.subject_to(constraints) # Maximum Distance of a constraint
+
+
+#         globalOptimizer.solver("ipopt")
+#         solution = globalOptimizer.solve()
+
+
+#         ###########################################################################################################
+#         # PRINT SOLUTION
+#         #########################################################################################################
+
+#         newFormulasCount = 0
+#         for i,j,edgeObject in optimisedEdges :
+#             newFormulasCount += len([formula for formula in edgeObject.formulasList if formula.isParametric])
+        
+        
+#         print("-----------------------------------------")   
+#         print("Internal Report")   
+#         print("-----------------------------------------")   
+#         print(f"Total number of formulas created : {newFormulasCount}")   
+#         print("---------Found Solution------------------") 
+#         for path,formulas in decompositionSolved :   
+#             print("path: ",path)
+#             for formula in formulas:
+#                 print("edge      : (" + str(formula.sourceNode)+ "," + str(formula.targetNode) + ")")
+#                 print("vector    : "+ str(solution.value(formula.centerVar)))
+#                 print("dimension : "+ str(solution.value(formula.nuVar)))
+#                 print("formua ID : " + str(id(formula)))
+#                 print("Operator  : "+ formula.temporalOperator)
+#                 # turn predicates from parameteric to no parameteric
+                
+                
+#     return TaskGraph
